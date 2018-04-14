d2d2f2f08eda66959dd7c2995233edc61787ddd5
aishwaryaanaidu <aishwarya13naidu@gmail.com>
2018-04-14 06:08:48 +0000

    added newsagentservice

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -11,5 +11,23 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class NewsAgentService {
+    public NewsAgentResponse getNewsAgentResponse(String message,UUID sessionId)
+    {
+        NewsAgentResponse newsAgentResponse = new NewsAgentResponse();
+        try{
+            WSRequest queryRequest=WS.url("https://api.api.ai/api/query");
+            CompletionStage<WSResponse> responsePromise=queryRequest
+                    .setQueryParameter("v","20150910")
+                    .setQueryParameter("query",query)
+                    .setQueryParameter("lang","en")
+                    .setQueryParameter("sessionId",sessionId.toString())
+                    .setQueryParameter("Authorization","Bearer 054a388ef08e46c3beb61cd9a12dd13f")
+                    .get();
+                    JsonNode response=responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
+        }
+        catch(Exception e){
+
+        }
+    }
 
 }
