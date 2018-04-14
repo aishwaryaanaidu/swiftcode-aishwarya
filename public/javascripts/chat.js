1c90b36b452b43f8b1c96064f7bcc32e6ea269a4
aishwaryaanaidu <aishwarya13naidu@gmail.com>
2018-04-14 12:12:22 +0000

    layout complete

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,9 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
-
-
-
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+        },
+        {
+            'sender': 'BOT',
+            'text': 'Hello what ca i do for you'
+        },
+        {
+            'sender': 'USER',
+            'text': 'Buy me a pizza'
+        },
+        {
+            'sender': 'BOT',
+            'text': 'Sure'
+        }
+    ];
 
 });
