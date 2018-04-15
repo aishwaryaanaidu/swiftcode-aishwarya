d9c6db38953e4aa0990173648f29ec38e7036409
aishwaryaanaidu <aishwarya13naidu@gmail.com>
2018-04-15 07:46:54 +0000

    wallpaper

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,9 @@
 var app = angular.module('chatApp', ['ngMaterial']);
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('purple')
+        .accentPalette('blue');
+});
 
 app.controller('chatController', function ($scope) {
     $scope.messages = [
